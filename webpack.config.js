6c18e6a10921a733a6ddcc76c4f8c375621ebf61
vc-dhruv <dhruv.prajapati@viitor.cloud>
2023-07-13 16:59:00 +0530

    feat: Create and refactor code in v1.0.0 branch
    In this commit, I created a new branch named v1.0.0 from the main branch. The v1.0.0 branch contains a complete overhaul of the codebase, introducing significant improvements and refactoring. This commit represents the initial implementation of the new code in the v1.0.0 branch.
    
    This commit addresses the following changes:
    
        Created the v1.0.0 branch from the main branch
        Introduced a comprehensive refactoring of the code
        Implemented new features and enhancements
    
    This commit aims to establish a solid foundation for future development in the v1.0.0 branch and reflects the substantial changes made to the codebase.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./dist/index.js",
   output: {
     filename: "everycred-verifier.js",
     path: path.resolve(__dirname, "dist"),
